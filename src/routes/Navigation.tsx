9022560bc5bca4806d4e4c40ca7a69d5a0f4176a
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-02-23 13:46:51 -0300

    proyecto inicial

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, NavLink, Navigate, Route, Routes } from "react-router-dom"
+import logo from '../assets/react.svg'
+
+
+
+export const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={logo} alt="react logo" />
+                    <ul>
+                        <li>
+                            <NavLink to='/home' className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/about' className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/users' className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="about" element={<h2>about page</h2>} />
+                    <Route path="users" element={<h2>users page</h2>} />
+                    <Route path="/" element={<h2>home page</h2>} />
+
+
+                    <Route path="/*" element={<Navigate to="/home" replace />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
+}
